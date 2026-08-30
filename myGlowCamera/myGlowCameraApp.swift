042fe07fbf8c407042764ed71c58c6fa43db6f12
marcos-v-costa <marcoscosta.marq@gmail.com>
2026-08-30 13:46:01 -0300

    Initial Commit

---
+++ myGlowCamera/myGlowCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  myGlowCameraApp.swift
+//  myGlowCamera
+//
+//  Created by marquiros on 30/08/26.
+//
+
+import SwiftUI
+
+@main
+struct myGlowCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
